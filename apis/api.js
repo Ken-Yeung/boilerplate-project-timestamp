c917c639ee0ed8adfeb43658f0e157aaa70a9368
Ken Yeung <yeungchunsiu@gmail.com>
2024-03-18 09:22:21 +0000

    added function

--- apis/api.js
+++ apis/api.js
@@ -5,15 +5,23 @@ app.get("/hello-world", function(req, res){
     res.send("Hello World")
 })
 
-app.get("/:date", function(req, res){
+// Writing Utils
+function getTheTime (req, res, next){
     const date = req.params.date
     var result;
     const respTemp = {
         unix: "",
         utc: ""
     }
-
     try {
+        if(!!!date){
+            const _newDate = new Date()
+            respTemp.unix = _newDate.getTime()
+            respTemp.utc = _newDate.toUTCString()
+            res.send(respTemp)
+            return
+        }
+
         if(date.includes("-")){
             result = new Date(date)
         } else {
@@ -24,18 +32,6 @@ app.get("/:date", function(req, res){
         if(result.toString() === "Invalid Date"){
             res.send({error: "Invalid Date"})
         } else {
-            // console.log("result:", result.toString());
-            // console.log("result:", typeof result.toString());
-            // console.log("result.toUTCString():", result.toUTCString());
-            // console.log("result.getDay():", result.getDay());
-            // console.log("result.getDate():", result.getDate());
-            // console.log("result.getMonth():", result.getMonth());
-            // console.log("result.getFullYear():", result.getFullYear());
-            // console.log("result.getHours():", result.getHours());
-            // console.log("result.getMinutes():", result.getMinutes());
-            // console.log("result.getSeconds():", result.getSeconds());
-            // console.log(result.getTime());
-
             respTemp.utc = result.toUTCString()
             respTemp.unix = result.getTime()
             // Result
@@ -45,6 +41,10 @@ app.get("/:date", function(req, res){
         console.error(err)
         res.status(422).send("Bad Request with worng params")
     }
-})
+}
+
+
+app.get("/", getTheTime)
+app.get("/:date", getTheTime)
 
 module.exports = app;
