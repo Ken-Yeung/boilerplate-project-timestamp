e4546f9df67aa20851b4efd5d582e07264570e7a
Ken Yeung <yeungchunsiu@gmail.com>
2024-03-18 09:42:06 +0000

    fixed some bug

--- apis/api.js
+++ apis/api.js
@@ -1,20 +1,21 @@
 var express = require('express');
 var app = express.Router();
 
-app.get("/hello-world", function(req, res){
+app.get("/hello-world", function (req, res) {
     res.send("Hello World")
 })
 
 // Writing Utils
-function getTheTime (req, res, next){
+function getTheTime(req, res, next) {
     const date = req.params.date
     var result;
     const respTemp = {
         unix: "",
         utc: ""
     }
+
     try {
-        if(!!!date){
+        if (!!!date) {
             const _newDate = new Date()
             respTemp.unix = _newDate.getTime()
             respTemp.utc = _newDate.toUTCString()
@@ -22,22 +23,22 @@ function getTheTime (req, res, next){
             return
         }
 
-        if(date.includes("-")){
+        if (date.includes("-") || date.includes(",") || date.includes(" ")) {
             result = new Date(date)
         } else {
             result = new Date(parseInt(date))
         }
 
         // Check is invalid date
-        if(result.toString() === "Invalid Date"){
-            res.send({error: "Invalid Date"})
+        if (result.toString() === "Invalid Date") {
+            res.send({ error: "Invalid Date" })
         } else {
             respTemp.utc = result.toUTCString()
             respTemp.unix = result.getTime()
             // Result
             res.send(respTemp)
         }
-    } catch (err){
+    } catch (err) {
         console.error(err)
         res.status(422).send("Bad Request with worng params")
     }
